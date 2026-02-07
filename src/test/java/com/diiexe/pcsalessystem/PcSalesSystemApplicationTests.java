c5f20f894d236e52dc14465e5278c7ed42a3d66c
Nguyexn Dii <nguyenduy.hd.0411@gmail.com>
2026-02-07 08:34:52 +0700

    first commit

---
+++ src/test/java/com/diiexe/pcsalessystem/PcSalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diiexe.pcsalessystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcSalesSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
